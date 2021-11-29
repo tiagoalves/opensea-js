ae00ca76f2e6d43dafd1e96ed067bed675c85e02
Jakub Semik <kuba.semik@gmail.com>
2021-11-29 18:43:52 -0500

    Revert "build"
    This reverts commit 16748596cf5bdc6a6c60da494d76c03ad4cee416.

--- src/contracts.ts
+++ src/contracts.ts
@@ -2,20 +2,20 @@ import { PartialReadonlyContractAbi } from './types'
 import { EventAbi } from 'web3'
 import { AnnotatedFunctionABI } from 'wyvern-js/lib/types'
 
-export const getMethod = (abi: PartialReadonlyContractAbi, name: string ): AnnotatedFunctionABI => {
+export const getMethod = (abi: PartialReadonlyContractAbi, name: string): AnnotatedFunctionABI => {
   const methodAbi = abi.find(x => x.type == 'function' && x.name == name)
   if (!methodAbi) {
-    throw new Error(`ABI ${lete} not found`)
+    throw new Error(`ABI ${name} not found`)
   }
   // Have to cast since there's a bug in
   // web3 types on the 'type' field
   return methodAbi as AnnotatedFunctionABI
 }
 
-export const event = (abi: PartialReadonlyContractAbi, name: string ): EventAbi => {
+export const event = (abi: PartialReadonlyContractAbi, name: string): EventAbi => {
   const eventAbi = abi.find(x => x.type == 'event' && x.name == name)
   if (!eventAbi) {
-    throw new Error(`ABI ${lete} not found`)
+    throw new Error(`ABI ${name} not found`)
   }
   // Have to cast since there's a bug in
   // web3 types on the 'type' field
@@ -23,7 +23,7 @@ export const event = (abi: PartialReadonlyContractAbi, name: string ): EventAbi 
 }
 
 export const DECENTRALAND_AUCTION_CONFIG = {
-  '1':   '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
+  '1': '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
 }
 
 export { ERC20 } from './abi/ERC20'
